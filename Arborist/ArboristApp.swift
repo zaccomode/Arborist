2d769b8e7a89939239a6a0138f2a6b52381d8784
Isaac Shea <isaac_shea@icloud.com>
2026-02-02 20:27:58 +1000

    Initial Commit

---
+++ Arborist/ArboristApp.swift
@@ -0,0 +1,17 @@
+//
+//  ArboristApp.swift
+//  Arborist
+//
+//  Created by Isaac Shea on 2/2/2026.
+//
+
+import SwiftUI
+
+@main
+struct ArboristApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ ArboristTests/ArboristTests.swift
@@ -0,0 +1,17 @@
+//
+//  ArboristTests.swift
+//  ArboristTests
+//
+//  Created by Isaac Shea on 2/2/2026.
+//
+
+import Testing
+@testable import Arborist
+
+struct ArboristTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
